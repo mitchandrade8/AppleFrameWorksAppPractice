b400ceddc2f351e9d951a18de88e1384edc89ffe
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-25 15:56:17 -0600

    --Challenge: attempted to build the unrefactored VStack to start building out the UI

--- AppleFrameWorksAppPractice/FrameworkGridView.swift
+++ AppleFrameWorksAppPractice/FrameworkGridView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let sample = MockData.sampleFramework
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(sample.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+            
+            Text(sample.imageName.capitalized)
+                .font(.system(size: 32, weight: .bold, design: .default))
+        }
     }
 }
 
 #Preview {
     FrameworkGridView()
 }
+
