7250421ccdf153a6843bf2ea63b8e8017bad4792
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-25 15:46:53 -0600

    --Progress: Deleted ContentView, created FrameworkGridView, added app icon images in assets

---
+++ AppleFrameWorksAppPractice/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/25/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
