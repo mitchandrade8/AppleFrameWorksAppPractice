16412745a1f8d55d1113d8f1d45826fa24c9c321
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-25 15:25:54 -0600

    Initial Commit

---
+++ AppleFrameWorksAppPractice/AppleFrameWorksAppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameWorksAppPracticeApp.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameWorksAppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameWorksAppPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
