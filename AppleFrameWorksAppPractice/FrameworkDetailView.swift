47510778f8fc91184775d85eef4a76c289df88fb
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-26 15:26:54 -0600

    --Challenge: Did things totally different from SA, his way is much simplier compared to mine

---
+++ AppleFrameWorksAppPractice/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/26/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
