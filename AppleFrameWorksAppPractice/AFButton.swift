62bd76d4ff7109e3e741f41f11798b329cabbd46
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-26 15:35:24 -0600

    --Progress: Refactored the button to its own reusable view

---
+++ AppleFrameWorksAppPractice/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/26/23.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
