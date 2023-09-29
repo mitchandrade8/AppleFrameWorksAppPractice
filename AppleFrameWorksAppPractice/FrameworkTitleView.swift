de2299a9f683f1deb94686ddb1f2a87cf55de2b1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-29 16:27:17 -0600

    --Progress: Refactoring making all our views simple and no logic

---
+++ AppleFrameWorksAppPractice/FrameworkTitleView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkTitleView.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/29/23.
+//
+
+import SwiftUI
+
+struct FrameworkTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkTitleView()
+}
