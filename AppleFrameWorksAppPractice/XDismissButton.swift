e5e5cde42f8b32662d3f896c248e998d01631713
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-30 14:33:34 -0600

    --Refactor: extracted the XDismissButton and added a binding to listen for the boolean changes

---
+++ AppleFrameWorksAppPractice/XDismissButton.swift
@@ -0,0 +1,18 @@
+//
+//  XDismissButton.swift
+//  AppleFrameWorksAppPractice
+//
+//  Created by Mitch Andrade on 9/30/23.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XDismissButton()
+}
